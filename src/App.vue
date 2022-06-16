0a50ceded710508233043daadc804ee8fc8f1941
KimJunSeop99 <99378782+KimJunSeop99@users.noreply.github.com>
2022-06-16 17:48:28 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">bezKoder</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/tutorials" class="nav-link">Tutorials</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Add</router-link>
+        </li>
+      </div>
+
+<!-- 로그인 태그 추가 -->
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+<!--            웹 아이콘 추가-->
+            <font-awesome-icon icon="user"></font-awesome-icon>
+            {{ currentUser.username }}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a href @click.prevent="logOut" class="nav-link">
+            <font-awesome-icon icon="sign-out-alt"></font-awesome-icon>
+            LogOut
+          </a>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    currentUser() {
+      // 공유저장소의 전역변수 ( 공유변수 : user )
+      // 자동으로 로그인되었으면 loggedIn = true
+      // 아니면 loggedIn = false , user 객체 = null
+      // return this.$store.state.auth.user;
+      return true; // 테스트용
+    }
+  },
+  // html에서 함수처럼 호출
+  methods: {
+    logOut() {
+      // dispatch 호출하는 메소드 : actions 있는 메소드를 호출함
+      this.$store.dispatch("auth/logout");
+      // 로그아웃 후 이동할 페이지 지정
+      this.$router.push("/login")
+    }
+  }
+};
+</script>
+
+
+
+
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+
+</template>
+
+<script>
+import User from '@/models/user'
+
+export default {
+  /* eslint-disable */
+  name: "Login",
+  data() {
+    return{
+      user: new User("",""),
+      loading: false,
+      message: ""
+    }
+  },
+  computed: {
+    loggedIn(){
+      return this.$store.state.auth.status.loggedIn;
+    }
+  },
+  created() {
+    // 로그인이 되었으면 loggedIn = true
+    // 로그인이 안되었으면 loggedIn = false
+    // 로그인이 되었으면 사용자를 프로필 페이지로 안내
+    if (this.loggedIn) {
+      // 강제 profile 페이지로 강제 이동함
+      this.$router.push("/profile");
+    }
+  },
+
+  methods: {
+
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
